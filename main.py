4831ff8625adc2ae8673e5323fce2bbdf20d4af1
plun1331 <49261529+plun1331@users.noreply.github.com>
2021-07-03 12:58:43 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+class Main:
+    def __init__(self) -> None:
+        self.server: object = None
+            
+    def on_load(self):
+        """Called whenever the plugin is loaded."""
+        ...
+
+    def on_unload(self):
+        """Called whenever the plugin is unloaded."""
+        ...
